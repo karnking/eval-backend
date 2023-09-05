508ef61f7181d4c24167dade40824dd303a31ddd
karnking <karankotai9183@gmail.com>
2023-09-05 13:30:39 +0530

    required

--- models/Blog.model.js
+++ models/Blog.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-    Title: String,
-    Content: String,
-    Author: String,
-    Category: String,
-    Image: String,
-    UserId: String
+    Title: {type:String,required:true},
+    Content: {type:String,required:true},
+    Author: {type:String,required:true},
+    Category: {type:String,required:true},
+    Image: {type:String,required:true},
+    UserId: {type:String,required:true}
 })
 
 const BlogModel = mongoose.model("blog",blogSchema)
